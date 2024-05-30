691e3942b5a527c951167332123b08085004a27d
axlschtn <schouten.axel@gmail.com>
2024-05-31 01:34:07 +0200

    EmailContent BaseModel

--- src/email/router.py
+++ src/email/router.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter, Request
+from pydantic import BaseModel
 
 email_router = APIRouter(
     prefix='/email',
     tags=['Email']
 )
 
+class EmailContent(BaseModel):
+    html: str
+
 @email_router.post('/parse')
 async def parse_email(
+    email: EmailContent,
     req: Request
 ):
-    print(req.body)
+    print(email)
     return 'ok'
