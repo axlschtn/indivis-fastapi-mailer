c1bae19099666cb891b631549fdbe4060b00c9dc
axlschtn <schouten.axel@gmail.com>
2024-05-31 13:06:20 +0200

    import regex for beinici / seloger title

--- src/email/router.py
+++ src/email/router.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from fastapi import APIRouter, Request, File
 import base64
 from bs4 import BeautifulSoup
+import re
 
 email_router = APIRouter(
     prefix='/email',
@@ -17,4 +18,8 @@ async def parse_email(
     file_str = base64.b64decode(file_decode).decode("utf-8")
     soup = BeautifulSoup(file_str, 'html.parser') 
     title_tag:str =  soup.find('title').text
-    return title_tag
+    bienici_pattern_title = r'II3-\d{4}'
+    bienici_matcher = re.findall(bienici_pattern_title, title_tag)
+    if bienici_matcher:
+        return f"BienIci {title_tag}"
+    return f"Seloger"
