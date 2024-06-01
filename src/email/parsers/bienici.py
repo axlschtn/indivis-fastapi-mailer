2ff7b46d0c46ee471d82bbf38d2792d522647045
axlschtn <schouten.axel@gmail.com>
2024-06-02 01:40:32 +0200

    Update

--- src/email/parsers/bienici.py
+++ src/email/parsers/bienici.py
@@ -6,12 +6,12 @@ def parser(soup: HTMLFormatter):
     response = ResponseParser()
     strong_tags = soup.find_all('strong')[1:5]
     for strong_tag in strong_tags:
-        if strong_tag.find('a'):
+        if not strong_tag.find('a').get('href').startswith('mailto:'):
             response.link_annonce = strong_tag.find('a').get("href")
         elif regex_result('phone', strong_tag.text):
              response.phone = strong_tag.text
         elif regex_result('email', strong_tag.text):
-            response.email = strong_tag.text
+            response.email = strong_tag.find('a').text
         else:
             response.user = strong_tag.text
     response.provider = 'bienici'
