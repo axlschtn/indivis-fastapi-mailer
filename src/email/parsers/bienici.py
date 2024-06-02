0827163045dddbbf3daa54823373e9f49d3bf876
axlschtn <schouten.axel@gmail.com>
2024-06-02 15:03:46 +0200

    update email parser

--- src/email/parsers/bienici.py
+++ src/email/parsers/bienici.py
@@ -1,15 +1,15 @@
 from bs4.formatter import HTMLFormatter
 from src.email.regex import regex_result
-from src.email.schema import ResponseParser
+from src.email.schema import ResponseParser, BienIciParser
 
 def parser(soup: HTMLFormatter):
     response = ResponseParser()
+    parser = BienIciParser()
     strong_tags = soup.find_all('strong')[1:5]
-    print(strong_tags)
-    response.link_annonce = strong_tags[0].find('a').get("href")
-    response.user = strong_tags[1].text
-    response.phone = strong_tags[2].text
-    response.email = strong_tags[3].text
+    parser.link_annonce = strong_tags[0].find('a').get("href")
+    parser.user = strong_tags[1].text
+    parser.phone = strong_tags[2].text
+    parser.email = strong_tags[3].text
     response.provider = 'bienici'
-    print(response)
+    response.bienIci = parser
     return response
--- src/email/schema.py
+++ src/email/schema.py
@@ -1,8 +1,17 @@
 from pydantic import BaseModel, Field
 
-class ResponseParser(BaseModel):
-    provider: str = Field(None)
+class BienIciParser(BaseModel):
     email: str = Field(None)
     phone: str = Field(None)
     user: str = Field(None)
     link_annonce: str = Field(None)
+    
+class VousAMoiParser(BaseModel):
+    email: str = Field(None)
+    user: str = Field(None)
+    link_annonce: str = Field(None)
+    
+class ResponseParser(BaseModel):
+    provider: str = Field(None)
+    bienIci: BienIciParser = Field(None)
+    vousAMoi: VousAMoiParser = Field(None)
