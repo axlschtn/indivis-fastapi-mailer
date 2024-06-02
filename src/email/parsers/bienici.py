a689203ac015855d21d241830035ad34be6845a6
axlschtn <schouten.axel@gmail.com>
2024-06-02 14:09:49 +0200

    update email parser

--- src/email/parsers/bienici.py
+++ src/email/parsers/bienici.py
@@ -5,7 +5,9 @@ from src.email.schema import ResponseParser
 def parser(soup: HTMLFormatter):
     response = ResponseParser()
     strong_tags = soup.find_all('strong')[1:5]
+    print(strong_tags)
     for strong_tag in strong_tags:
+        print(strong_tag)
         if strong_tag.find('a'):
             if strong_tag.find('a').get('href').startswith('mailto:'):
                 response.email = strong_tag.find('a').text    
@@ -16,4 +18,5 @@ def parser(soup: HTMLFormatter):
         else:
             response.user = strong_tag.text
     response.provider = 'bienici'
+    print(response)
     return response
