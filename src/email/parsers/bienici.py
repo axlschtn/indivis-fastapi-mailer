93c01498bef2d219cd2106e0d4767ca9f7fe6aa8
axlschtn <schouten.axel@gmail.com>
2024-06-02 14:17:05 +0200

    update email parser

--- src/email/parsers/bienici.py
+++ src/email/parsers/bienici.py
@@ -6,17 +6,10 @@ def parser(soup: HTMLFormatter):
     response = ResponseParser()
     strong_tags = soup.find_all('strong')[1:5]
     print(strong_tags)
-    for strong_tag in strong_tags:
-        print(strong_tag)
-        if strong_tag.find('a'):
-            if strong_tag.find('a').get('href').startswith('mailto:'):
-                response.email = strong_tag.find('a').text    
-            else:
-                response.link_annonce = strong_tag.find('a').get("href")
-        elif regex_result('phone', strong_tag.text):
-            response.phone = strong_tag.text
-        else:
-            response.user = strong_tag.text
+    response.link_annonce = strong_tags[0].find('a').get("href")
+    response.user = strong_tags[1].text
+    response.phone = strong_tags[2].text
+    response.email = strong_tags[3].text
     response.provider = 'bienici'
     print(response)
     return response
