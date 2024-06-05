6a9682c76bb9b11dc26335cdb138d1706d04552e
axlschtn <schouten.axel@gmail.com>
2024-06-05 16:00:27 +0200

    Update project

--- src/email/router.py
+++ src/email/router.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, File, Depends
+from fastapi import APIRouter, File, Depends, HttpException
 import base64
 from bs4 import BeautifulSoup
 from src.core.regex import extract_domain_email
@@ -26,10 +26,15 @@ async def parse_email(
         file_decode = file.decode("utf-8").encode('utf-8')
         file_str = base64.b64decode(file_decode).decode("utf-8")
         soup = BeautifulSoup(file_str, "html.parser")
-        if email == 'bienici':
-            return ImplBieniciParser(soup=soup).execute()
-        if email == 'vousamoi' or email == 'ambrabbyhub':
-            return ImplVousAmoiParser(soup=soup).execute()
+        try:
+            if email == 'bienici':
+                print(ImplBieniciParser(soup=soup).execute())
+                return ImplBieniciParser(soup=soup).execute()
+            if email == 'vousamoi' or email == 'ambrabbyhub':
+                print(ImplVousAmoiParser(soup=soup).execute())
+                return ImplVousAmoiParser(soup=soup).execute()
+        except ValueError as error:
+            print(str(error))
     else:
         if email == 'bienici':
             print(ImplBieniciParser(soup=soup_mock).execute())
